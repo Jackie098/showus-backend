b9c969ec130da523ec860f42f6c5fd9e7bf82745
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-27 19:02:17 -0300

    configured sentry

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
+import * as Sentry from '@sentry/node';
 
 import routes from './router';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -11,17 +13,23 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(cors());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
