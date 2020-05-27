d3f3b82ce197a0c5a3fc5230d069e4b98663a048
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-27 19:48:19 -0300

    created companies migration

---
+++ src/database/migrations/20200527221411-create-companies.js
@@ -0,0 +1,43 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('companies', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      whatsapp: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      instagram: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('companies');
+  },
+};
