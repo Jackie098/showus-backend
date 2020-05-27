ae66fe77b615aac262e8e069cbff8af35dd67719
Jackie098 <carlosmiranda19122@gmail.com>
2020-05-27 19:11:03 -0300

    configured youch

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import 'dotenv/config';
 
 import express from 'express';
 import cors from 'cors';
+
+import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 
 import routes from './router';
 import sentryConfig from './config/sentry';
@@ -30,6 +33,18 @@ class App {
     this.server.use(routes);
 
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
