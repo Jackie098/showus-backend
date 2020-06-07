6b350e44bb7715783881be9e4ba38f1acd58033c
Jackie098 <carlosmiranda19122@gmail.com>
2020-06-07 12:02:56 -0300

    companyCardController: created index function

---
+++ src/app/controllers/CompanyCardController.js
@@ -0,0 +1,31 @@
+import Company from '../models/Company';
+import File from '../models/File';
+
+class CompanyCardController {
+  async index(req, res) {
+    const companies = await Company.findAll();
+
+    if (!companies) {
+      return res.status(200).json();
+    }
+
+    const files = await File.findAll();
+
+    const cards = companies.map((company) => {
+      // onde company_id(files) = id (company) adiciona um novo item no vetor de cards
+
+      const filesCompany = files.filter(
+        (file) => file.company_id === company.id
+      );
+
+      return {
+        company,
+        filesCompany,
+      };
+    });
+
+    return res.json(cards);
+  }
+}
+
+export default new CompanyCardController();
--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import CompanyComtroller from './app/controllers/CompanyController';
+import CompanyController from './app/controllers/CompanyController';
 import FileController from './app/controllers/FileController';
 import ProductTypeController from './app/controllers/ProductTypeController';
 import ProductController from './app/controllers/ProductController';
 import SizeController from './app/controllers/SizeController';
 import MenuController from './app/controllers/MenuController';
+import CompanyCardController from './app/controllers/CompanyCardController';
 
 import checkIdCompany from './app/middlewares/checkIdcompany';
 import maxAmount from './app/middlewares/maxAmount';
@@ -19,10 +20,10 @@ const upload = multer(multerConfig);
 /**
  * To manipule Company
  */
-routes.get('/company', CompanyComtroller.index);
-routes.post('/company', CompanyComtroller.store);
-routes.put('/company/:id', CompanyComtroller.update);
-routes.delete('/company/:id', CompanyComtroller.delete);
+routes.get('/company', CompanyController.index);
+routes.post('/company', CompanyController.store);
+routes.put('/company/:id', CompanyController.update);
+routes.delete('/company/:id', CompanyController.delete);
 
 /**
  * To manipule Files
@@ -63,5 +64,10 @@ routes.delete('/size/:id', SizeController.delete);
  */
 routes.put('/menu/:id', MenuController.update);
 routes.delete('/menu/:id', MenuController.delete);
+
+/**
+ * To manipule Company Cards
+ */
+routes.get('/card', CompanyCardController.index);
 
 export default routes;
